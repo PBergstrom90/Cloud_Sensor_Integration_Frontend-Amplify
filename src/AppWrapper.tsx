c4159e8402ba7b2223628528757049f490e91792
Pontus Bergström <pontusbergstrom90@gmail.com>
2024-11-29 15:07:46 +0100

    Removed unused variable

--- src/AppWrapper.tsx
+++ src/AppWrapper.tsx
@@ -1,5 +1,4 @@
 import React from "react";
-import ReactDOM from "react-dom";
 import { ThemeProvider, createTheme, CssBaseline } from "@mui/material";
 import App from "./App";
 import ErrorBoundary from "./ErrorBoundary";
